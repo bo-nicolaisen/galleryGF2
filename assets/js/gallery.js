fc98b13b2e4720b2b8e387c4bd3289e4dcb3a682
Bo Nicolajsen <bni@techcollege.dk>
2023-05-22 13:51:57 +0200

    jy
    io

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -9,34 +9,69 @@ window.addEventListener("load", generateGrid);
 
 // globale variabler
 // let myData = null; //til data objekt opgaven
-const myImages = []; // til array opgaven, byg dit array af billed urlér her.
+const myImages = ['assets/img/Brachypelma_smithi.jpg', 'assets/img/elephant.jpg', 'assets/img/koala.jpg', 'assets/img/great-white.jpg']; // til array opgaven, byg dit array af billed urlér her.
 const myApp = document.getElementById("app");
 
 
 
 function generateGrid() {
+    clearApp();
+
+    myImages.map((imageUrl) => {
+        console.log('map');
+        createCard(imageUrl);
+    });
 
 }
 
 
 
 function createCard(myUrl) {
+    console.log('create card');
+    let myArticleTag = document.createElement('article');
+    myArticleTag.classList.add("galleryCard");
+    let myImageTag = document.createElement('img');
+    myImageTag.src = myUrl;
+    myImageTag.addEventListener('click', clickSmall);
+
+
+    myArticleTag.appendChild(myImageTag);
+
+    myApp.appendChild(myArticleTag);
 
 }
+
+
 
 
 /* find url på det klikkede image
    send url til build big */
 function clickSmall(myEvent) {
+    let myUrl = myEvent.target.src;
+    buildBig(myUrl);
 
 }
 
 function buildBig(myUrl) {
+    clearApp();
 
+    console.log('create big');
+    let myArticleTag = document.createElement('article');
+
+    myArticleTag.classList.add("detailView");
+
+    let myImageTag = document.createElement('img');
+    myImageTag.src = myUrl;
+    myImageTag.addEventListener('click', generateGrid);
+
+
+    myArticleTag.appendChild(myImageTag);
+
+    myApp.appendChild(myArticleTag);
 
 }
 
 function clearApp() {
-
+    myApp.innerHTML = "";
 }
 
